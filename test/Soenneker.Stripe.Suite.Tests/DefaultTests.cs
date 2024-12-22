2140ea0cc5b3afb0d83f1d1ec71cb623d8599c0b
Jake Soenneker <jake@soenneker.com>
2024-12-22 08:45:00 -0600

    Tests

--- test/Soenneker.Stripe.Suite.Tests/DefaultTests.cs
+++ test/Soenneker.Stripe.Suite.Tests/DefaultTests.cs
@@ -1,4 +1,3 @@
-using Soenneker.Stripe.Suite.Abstract;
 using Soenneker.Tests.FixturedUnit;
 using Xunit;
 
@@ -7,11 +6,8 @@ namespace Soenneker.Stripe.Suite.Tests;
 [Collection("Collection")]
 public class DefaultTests : FixturedUnitTest
 {
-    private readonly IDefault _util;
-
     public DefaultTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
     {
-        _util = Resolve<IDefault>(true);
     }
 
     [Fact]
